d63290421b61e779789dc58fe09dfc7681035512
Josiane <jcarolliny@gmail.com>
2021-12-07 11:02:37 -0300

    @JosianeCaroliny [Task 1: Criação do projeto Loja de Games em Spring]

---
+++ src/main/java/com/minhaLojaDeGames/PlayGen/PlayGenApplication.java
@@ -0,0 +1,13 @@
+package com.minhaLojaDeGames.PlayGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minhaLojaDeGames/PlayGen/PlayGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhaLojaDeGames.PlayGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
